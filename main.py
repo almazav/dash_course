b68de6ab697ae64a16e96a99bb276c6491ffb821
almazav <alemazav1002@gmail.com>
2021-07-29 13:53:48 +0200

    cards and indicators added

--- main.py
+++ main.py
@@ -52,6 +52,9 @@ covid_data['recovered'] = covid_data['recovered'].fillna(0)
 covid_data['active'] = covid_data['confirmed'] - (covid_data['deaths']+covid_data['recovered'])
 
 covid_data_1 = covid_data.groupby(['date'])[['confirmed','deaths','recovered','active']].sum().reset_index()
+
+
+
 #######################################################################################################################
 #                                       DASHBOARD
 #######################################################################################################################
@@ -93,11 +96,126 @@ app.layout = html.Div([
                    style={'textAlign': 'center',
                           'color': 'orange',
                           'fontSize':40}
-        )
+        ),
+            html.P("new: "+ f"{(covid_data_1['confirmed'].iloc[-1]- covid_data_1['confirmed'].iloc[-2]):.0f}"
+                   + " (" + str(round((covid_data_1['confirmed'].iloc[-1]- covid_data_1['confirmed'].iloc[-2])/
+                                      (covid_data_1['confirmed'].iloc[-1]) * 100, 2)) +"%)",
+                   style={'textAlign':'center',
+                          'color':'orange',
+                          'fontSize':15,
+                          'margin-top':'-18px'})
+
+        ], className='card_container three columns'),
+        html.Div([
+            html.H6(children='Global Deaths',
+                    style={'textAlign': 'center',
+                           'color': 'white'}),
+            html.P(f"{covid_data_1['deaths'].iloc[-1]:.0f}",
+                   style={'textAlign': 'center',
+                          'color': '#dd1e35',
+                          'fontSize': 40}
+                   ),
+            html.P("new: " + f"{(covid_data_1['deaths'].iloc[-1] - covid_data_1['deaths'].iloc[-2]):.0f}"
+                   + " (" + str(round((covid_data_1['deaths'].iloc[-1] - covid_data_1['deaths'].iloc[-2]) /
+                                      (covid_data_1['deaths'].iloc[-1]) * 100, 2)) + "%)",
+                   style={'textAlign': 'center',
+                          'color': '#dd1e35',
+                          'fontSize': 15,
+                          'margin-top': '-18px'})
+
+        ], className='card_container three columns'),
+        html.Div([
+            html.H6(children='Global Recovered',
+                    style={'textAlign': 'center',
+                           'color': 'white'}),
+            html.P(f"{covid_data_1['recovered'].iloc[-1]:.0f}",
+                   style={'textAlign': 'center',
+                          'color': 'green',
+                          'fontSize': 40}
+                   ),
+            html.P("new: " + f"{(covid_data_1['recovered'].iloc[-1] - covid_data_1['recovered'].iloc[-2]):.0f}"
+                   + " (" + str(round((covid_data_1['recovered'].iloc[-1] - covid_data_1['recovered'].iloc[-2]) /
+                                      (covid_data_1['recovered'].iloc[-1]) * 100, 2)) + "%)",
+                   style={'textAlign': 'center',
+                          'color': 'green',
+                          'fontSize': 15,
+                          'margin-top': '-18px'})
+
+        ], className='card_container three columns'),
+        html.Div([
+            html.H6(children='Global Active',
+                    style={'textAlign': 'center',
+                           'color': 'white'}),
+            html.P(f"{covid_data_1['active'].iloc[-1]:.0f}",
+                   style={'textAlign': 'center',
+                          'color': '#e55467',
+                          'fontSize': 40}
+                   ),
+            html.P("new: " + f"{(covid_data_1['active'].iloc[-1] - covid_data_1['active'].iloc[-2]):.0f}"
+                   + " (" + str(round((covid_data_1['active'].iloc[-1] - covid_data_1['active'].iloc[-2]) /
+                                      (covid_data_1['active'].iloc[-1]) * 100, 2)) + "%)",
+                   style={'textAlign': 'center',
+                          'color': '#e55467',
+                          'fontSize': 15,
+                          'margin-top': '-18px'})
 
         ], className='card_container three columns')
+
+    ], className='row flex display'),
+    html.Div([
+        html.Div([
+            html.P('Select Country:', className='fix_label',style={'color':'white'}),
+            dcc.Dropdown(id ='w_countries',
+                         multi=False,
+                         searchable=True,
+                         value='US',
+                         placeholder='Select Countries',
+                         options=[{'label':c,'value':c}
+                                  for c in (covid_data['Country/Region'].unique())],className='dcc_compon'),
+            html.P('New Cases: ' + ' ' + str(covid_data['date'].iloc[-1].strftime('%B %d %Y')),
+                   className='fix_label', style={'test-align': 'center','color':'white'}),
+            dcc.Graph(id= 'confirmed',config={'displayModeBar':False}, className='dcc_compon',
+                      style={'margin-top':'20px'})
+
+        ],className='create_container three columns')
     ], className='row flex display')
 ], id='mainContainer',style={'display': 'flex', 'flex-direction':'column'})
+
+@app.callback(Output('confirmed','figure'),
+              [Input('w_countries','value')])
+def update_confirmed(w_countries):
+    covid_data_2 = covid_data.groupby(['date', 'Country/Region'])[
+        ['confirmed', 'deaths', 'recovered', 'active']].sum().reset_index()
+    value_confirmed = covid_data_2[covid_data_2['Country/Region'] == w_countries]['confirmed'].iloc[-1] - \
+                      covid_data_2[covid_data_2['Country/Region'] == w_countries]['confirmed'].iloc[-2]
+    delta_confirmed = covid_data_2[covid_data_2['Country/Region'] == w_countries]['confirmed'].iloc[-2] - \
+                      covid_data_2[covid_data_2['Country/Region'] == w_countries]['confirmed'].iloc[-3]
+
+    return {
+        'data':[go.Indicator(
+            mode='number+delta',
+            value=value_confirmed,
+            delta={'reference':delta_confirmed,
+                   'position':'right',
+                   'valueformat':',g',
+                   'relative':False,
+                   'font':{'size':15}},
+            number={'valueformat': ',',
+                    'font' : {'size':20}},
+            domain={'y': [0, 1], 'x':[0, 1]}
+        )],
+        'layout':go.Layout(
+            title={'text':'New Conirmed',
+                   'y':1,
+                   'x': 0.5,
+                   'xanchor': 'center',
+                   'yanchor': 'top'},
+            font=dict(color= 'orange'),
+            paper_bgcolor='#1f2c56',
+            plot_bgcolor='#1f2c56',
+            height=50,
+        )
+    }
 
 if __name__== '__main__':
     app.run_server(debug=True)
